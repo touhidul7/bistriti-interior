0d8f6c1462f7d22e7e83e8b705879b5eb5814b47
touhidul-islam-html <ti360109@gmail.com>
2024-11-04 23:40:22 +0600

    update

--- app/page.js
+++ app/page.js
@@ -51,14 +51,7 @@ const page = () => {
       <section className="bg-black dark:bg-gray-900">
         <div className="container px-16 py-5 mx-auto">
           <div className="relative z-10 flex flex-col justify-center items-center h-full text-center mt-0">
-            {/* <h1 className="text-9xl font-bold  text-white -mb-16 opacity-[6%]">
-              RECENT WORK
-            </h1>
-            <h2 className="text-8xl font-semibold  text-white mb-10 fade-in2">
-              RECENT WORK
-            </h2> */}
             <Sectionheading title={"RECENT WORK"} />
-
             <section className="py-10 sm:py-16 lg:py-10">
               <div className="px-4 mx-auto sm:px-6 lg:px-2 max-w-7xl">
                 <div className="container mx-auto px-2 py-8">
