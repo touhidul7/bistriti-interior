06b8ae9e63f2a867213a6f018c6b211b5e452a75
unknown <noman40046@gmail.com>
2024-11-06 16:08:41 +0600

    respoonsive

--- app/Components/Homecomponents/Landmark.js
+++ app/Components/Homecomponents/Landmark.js
@@ -4,43 +4,38 @@ import landmarkimg from "@/public/Landmark.png";
 import { motion } from "framer-motion";
 export default function Landmark() {
   return (
-    <div className="bg-black pt-28">
-      <div className="heading text-center text-6xl font-bold text-white leading-normal">
+    <div className="bg-black pt-20 lg:pt-28">
+      <div className="heading text-center text-3xl lg:text-6xl font-bold text-white leading-normal">
         Witness, As We <br /> Transform Your Land <br /> to a Land Mark
       </div>
-      <div className="flex justify-center items-center py-10">
-        <div className="left-sidebar flex flex-col justify-between pb-20 min-h-screen items-center">
-          <div className="text-right">
-            <div className="text-4xl font-semibold text-yellow-400">7M+</div>
-            <div className="text-white text-lg font-semibold">
+      <div className="flex justify-center items-center lg:py-10 px-5">
+        <div className="left-sidebar flex flex-col lg:justify-between  pb-20 h-[400px] lg:min-h-screen items-center">
+          <div className="text-right mb-10 mt-20">
+            <div className="text-2xl lg:text-4xl font-semibold text-yellow-400">7M+</div>
+            <div className="text-white text-sm lg:text-lg font-semibold">
               Total Area Build
             </div>
-            <div className="text-white text-lg font-semibold">
-              {"(Million Sft.)"}
-            </div>
           </div>
-          <div className="text-right">
-            <div className="text-4xl font-semi text-yellow-400">07</div>
-            <div className="text-white text-lg font-semibold">
-              Years Since Inception
-            </div>
 
-          </div>
-          <div className="text-right">
-            <div className="text-4xl font-semibold text-yellow-400">42</div>
-            <div className="text-white text-lg font-semibold">
-              Number of Completed
+          <div className="text-right mb-10">
+            <div className="text-2xl lg:text-4xl font-semibold text-yellow-400">07</div>
+            <div className="text-white text-sm lg:text-lg font-semibold">
+              Years Inception
             </div>
-            <div className="text-white text-lg font-semibold">
-              Projects
+          </div>
+
+          <div className="text-right">
+            <div className="text-2xl lg:text-4xl font-semibold text-yellow-400">42</div>
+            <div className="text-white text-sm lg:text-lg font-semibold">
+              Number of Completed Projects
             </div>
           </div>
         </div>
         <div className="image">
           <motion.div
             className="w-fit lg:mt-10"
-            initial={{ y: 300 }}
-            whileInView={{ y: 0 }}
+            initial={{ y: 200 }}
+            whileInView={{ y:50 }}
             transition={{
               duration: 1,
               delay: 0.1,
@@ -51,26 +46,23 @@ export default function Landmark() {
           </motion.div>
         </div>
 
-        <div className="right-sidebar flex flex-col justify-between pb-20 min-h-screen items-center">
-          <div className="text-left">
-            <div className="text-4xl font-semibold text-yellow-400">100+</div>
-            <div className="text-white text-lg font-semibold">
+        <div className="right-sidebar flex flex-col lg:justify-between   pb-20 h-[400px] lg:min-h-screen  items-center">
+          <div className="text-left mb-10 mt-20">
+            <div className="text-2xl lg:text-4xl font-semibold text-yellow-400">100+</div>
+            <div className="text-white text-sm lg:text-lg font-semibold">
               Number of Prejects
             </div>
           </div>
-          <div className="text-left">
-            <div className="text-4xl font-semibold text-yellow-400">1100+</div>
-            <div className="text-white text-lg font-semibold">
+          <div className="text-left mb-10">
+            <div className="text-2xl lg:text-4xl font-semibold text-yellow-400">1100+</div>
+            <div className="text-white text-sm lg:text-lg font-semibold">
               Happy Clients
             </div>
           </div>
           <div className="text-left">
-            <div className="text-4xl font-semibold text-yellow-400">11M+</div>
-            <div className="text-white text-lg font-semibold">
+            <div className="text-2xl lg:text-4xl font-semibold text-yellow-400">11M+</div>
+            <div className="text-white text-sm lg:text-lg font-semibold">
               Total Area in Pipeline
-            </div>
-            <div className="text-white text-lg font-semibold">
-              {"(Million Sft.)"}
             </div>
           </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -11,29 +11,28 @@ const page = () => {
   return (
     <>
       <section
-        className="relative bg-cover bg-center h-screen flex items-end"
+        className="relative bg-cover bg-center h-[700px] lg:h-screen flex items-end"
         style={{
           backgroundImage: `url('../home-header4.jpg')`,
           backgroundAttachment: "fixed",
           backgroundSize: "cover",
         }}
-         
-      >
+        >
         <div className="relative text-start text-white">
-          <h1 className="text-4xl md:text-2xl bg-black w-[440px]  ps-20 pe-4 pt-[104px] text-[#BABABA] border-r-[12px] border-[#750000] pb-20">
+          <h1 className="text-[18px] lg:text-2xl md:text-2xl bg-black w-[200px] lg:w-[440px] ps-5 lg:ps-20 pe-6 lg:pe-4 pt-10 lg:pt-[104px] text-[#BABABA] border-r-[12px] border-[#750000] pb-24 lg:pb-20">
             We begin with in-depth consultations to capture your vision and
             preferences
           </h1>
-          <h1 className="text-4xl md:text-9xl  font-bold w-[540px] border-r-[12px] border-[#750000] ps-20 bg-black">
+          <h1 className="text-5xl md:text-9xl font-bold w-[240px] lg:w-[540px] border-r-[12px] border-[#750000] ps-5 lg:ps-20 bg-black">
             Design
           </h1>
-          <h1 className="text-4xl md:text-9xl font-bold w-[600px] ps-20 border-r-[12px]  border-[#750000] bg-black">
+          <h1 className="text-5xl md:text-9xl font-bold w-[290px] lg:w-[600px] lg:ps-20 border-r-[12px]  border-[#750000] ps-5  bg-black">
             Beyond
           </h1>
-          <h1 className="text-4xl md:text-9xl font-bold w-[1010px] pb-10 ps-20 border-r-[12px] border-[#750000] bg-black">
+          <h1 className="text-5xl md:text-9xl ps-5 lg:ps-20 font-bold w-[355px] lg:w-[1010px] pb-5 lg:pb-10  border-r-[12px] border-[#750000] bg-black">
             Boundaries
-            <span className="ml-9 item-center">
-              <button className="group relative min-h-[65px] w-48 overflow-hidden border border-white hover:border-[#750000]  transition-all before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#750000] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#750000] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full md:text-2xl">
+            <span className="ml-4 lg:ml-9 item-center">
+              <button className="group flex mt-5 lg:inline relative lg:min-h-[65px] min-h-[50px] lg:w-48 w-28 overflow-hidden border border-white hover:border-[#750000]  transition-all before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#750000] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#750000] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full md:text-2xl text-lg">
                 <span className="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#750000] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#750000] after:duration-500 hover:text-white group-hover:before:h-full group-hover:after:h-full"></span>
                 <span className="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-white text-[#750000]">
                   VISIT US
@@ -46,7 +45,7 @@ const page = () => {
 
 
       <Landmark />
-      <div className='py-10 '>
+      <div className='py-28'>
         <Sectionheading title={"Catagory"} />
         <Homecatagory />
       </div>
@@ -54,7 +53,7 @@ const page = () => {
 
       {/* recent work */}
       <section className="bg-black dark:bg-gray-900">
-        <div className="container px-16 py-5 mx-auto">
+        <div className="container lg:px-16 py-5 mx-auto">
           <div className="relative z-10 flex flex-col justify-center items-center h-full text-center mt-0">
             <Sectionheading title={"RECENT WORK"} />
             <section className="py-10 sm:py-16 lg:py-10">
